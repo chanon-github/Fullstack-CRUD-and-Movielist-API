31ca3578aef512b6ccd00e7424c2643229683a6b
ch <ch.chantaramat@gmail.com>
2023-08-07 15:47:31 +0700

    deploy

--- db/connection.js
+++ db/connection.js
@@ -1,14 +1,25 @@
 const mariadb = require("mariadb");
 require("dotenv").config();
 const { DB_HOSTNAME, DB_PASSWORD, DB_USERNAME, DB_PORT, DB_NAME } = process.env;
+// const pool = mariadb.createPool({
+//   host: DB_HOSTNAME,
+//   user: DB_USERNAME,
+//   password: DB_PASSWORD,
+//   port: DB_PORT,
+//   database: DB_NAME,
+//   connectionLimit: 5, // Adjust the connection limit as needed
+// });
+
 const pool = mariadb.createPool({
-  host: DB_HOSTNAME,
-  user: DB_USERNAME,
-  password: DB_PASSWORD,
-  port: DB_PORT,
-  database: DB_NAME,
+  host: 'un0jueuv2mam78uv.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+  user: 'an4fuvb6qgbvaihy',
+  password: 'umzhfjouts7arze6',
+  port: '3306',
+  database: 'pzowcj8qn5zqp14y',
   connectionLimit: 5, // Adjust the connection limit as needed
 });
+
+
 
 pool.getConnection((err, conn) => {
   if (err) {
