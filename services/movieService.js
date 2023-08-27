0a12aa72aee18eacc3babd79f92f29ac1f176b27
ch <ch.chantaramat@gmail.com>
2023-08-27 20:20:54 +0700

    commit

--- services/movieService.js
+++ services/movieService.js
@@ -5,18 +5,16 @@ const { API_KEY, API_GETMOVIE_DOMAIN } = process.env;
 module.exports = {
   get: async (req, res) => {
     const id = req.query.id;
-    // let result = []
-
+    const page = req.query.page;
     if(id){
         const response = await fetch(`${API_GETMOVIE_DOMAIN}/${id}?api_key=${API_KEY}`)
         const result = await response.json(); 
         res.send(result)
     }else{
-        const response = await fetch(`${API_GETMOVIE_DOMAIN}/now_playing?api_key=${API_KEY}`)
+        const response = await fetch(`${API_GETMOVIE_DOMAIN}/now_playing?api_key=${API_KEY}&page=${page}`)
         const result = await response.json(); 
         res.send(result)
     }
-    // res.send('dddddddddd')
 
   }
 };
